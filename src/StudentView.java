1e0252e264638ca952d0e5d31f1d1e9b25499cc4
Mayela3018 <mayela.ticona@tecsup.edu.pe>
2025-08-29 21:48:29 -0500

    Corrijo StudentView directo en src

---
+++ src/StudentView.java
@@ -0,0 +1,5 @@
+public class StudentView {
+    public static void main(String[] args) {
+        System.out.println("Hola, soy StudentView directo ,may ✨");
+    }
+}
--- src/app/StudentView.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-public class StudentView {
-    public static void main(String[] args) {
-        System.out.println("Hola, soy StudentView subido por Maye ✨");
-    }
-}
